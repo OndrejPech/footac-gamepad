c411d9dda35a113c46de3942beae8e4426689081
Ondrej Pech <pech.ondrej@gmail.com>
2022-09-21 13:30:14 +0000

    Upload New Files

---
+++ config/config.py
@@ -0,0 +1,9 @@
+# color plate
+BLACK = (0, 0, 0)
+LIGHT_BLACK = (40, 40, 40)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+SEA_GREEN = (46, 139, 87)
+GREEN1 = (46, 147, 87)
+GREEN2 = (46, 100, 87)
+GREEN3 = (46, 110, 87)
