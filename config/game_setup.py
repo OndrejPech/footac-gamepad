fc630b2b0177ac86d43fca1e64ead1a5ee903c81
Ondrej Pech <pech.ondrej@gmail.com>
2022-09-21 13:30:39 +0000

    Upload New File

---
+++ config/game_setup.py
@@ -0,0 +1,19 @@
+HALF_TIME_DURATION = 2700  # in seconds
+
+video_file = '/Users/ondrejpech/Movies/zruc vs slavia.mp4'
+HOME_TEAM = 'Zruc'
+AWAY_TEAM = 'Unesov'
+
+# from DB
+GAME_ID = 10
+HOME_TEAM_ID = 2
+AWAY_TEAM_ID = 6
+
+HOME_TEAM_LEFT_PITCH = True  # by start game, auto-change if first_half is False
+first_half_start_video_time = '00:00'  # format MM:SS  at media player
+second_half_start_video_time = '46:00'  # format MM:SS  at media player
+
+#  !!! IMPORTANT USER SETUP need to update before every re-run!!!
+first_half = True
+# options to start timer at any time of the game
+timer_start_at = '00:00'  # format MM:SS
