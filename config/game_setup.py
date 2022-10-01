815bfe9b8dea5245ffd7fc8c3473f288f94901d5
OndrejPech <pech.ondrej@gmail.com>
2022-10-01 15:25:35 +0200

    previous commit had wrong description, I just changed game_setup data

--- config/game_setup.py
+++ config/game_setup.py
@@ -1,17 +1,17 @@
 HALF_TIME_DURATION = 2700  # in seconds
 
-video_file = '/Users/ondrejpech/Movies/zruc vs slavia.mp4'
+video_file = '/Users/ondrejpech/Movies/zruc  vs myto.mp4'
 HOME_TEAM = 'Zruc'
-AWAY_TEAM = 'Unesov'
+AWAY_TEAM = 'Myto'
 
 # from DB
-GAME_ID = 10
-HOME_TEAM_ID = 2
-AWAY_TEAM_ID = 6
+GAME_ID = 5
+HOME_TEAM_ID = 3
+AWAY_TEAM_ID = 4
 
 HOME_TEAM_LEFT_PITCH = True  # by start game, auto-change if first_half is False
 first_half_start_video_time = '00:00'  # format MM:SS  at media player
-second_half_start_video_time = '46:00'  # format MM:SS  at media player
+second_half_start_video_time = '45:37'  # format MM:SS  at media player
 
 #  !!! IMPORTANT USER SETUP need to update before every re-run!!!
 first_half = True
